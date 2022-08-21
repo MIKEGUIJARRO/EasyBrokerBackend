163c88702efbcc2808820167f01e557cd633015d
Mike Guijarro <a01612042@itesm.mx>
2022-08-20 21:36:09 -0500

    add properties controller logic

--- controllers/properties.js
+++ controllers/properties.js
@@ -1,8 +1,11 @@
-const asyncHandler = require("../middlewares/async")
+const asyncHandler = require("../middlewares/async");
+const EasyBrokerApi = require('../util/EasyBrokerApi');
 
 module.exports.getProperties = asyncHandler(async (req, res, next) => {
+    const ebApi = new EasyBrokerApi();
+    const response = await ebApi.properties('get');
     res.status(200).json({
-        success: true
-    })
-    console.log(e)
+        success: true,
+        data: response
+    });
 })
